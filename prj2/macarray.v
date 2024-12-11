4cc4df12b4d21a6f574a1ac3d8877d067f59044f
N1ELITE <junho990613@gmail.com>
2024-12-11 16:13:17 +0900

    수정본

--- prj2/macarray.v
+++ prj2/macarray.v
@@ -50,9 +50,19 @@ module macarray (
     reg [7:0] b41, b42, b43, b44;
 
 
+    reg [7:0] wb51 wb52 wb53 wb54;
+    reg [7:0] wb41 wb42 wb43 wb44;
+    reg [7:0] wb31 wb32 wb33 wb34;
+    reg [7:0] wb21 wb22 wb23 wb24;
+    reg [7:0] wb11 wb12 wb13 wb14;
+
+
+
+
     reg [3:0] readcount_I_flag;
     reg [3:0] readcount_W_flag;
 
+    reg weight_delay;
     reg error_flag;
 
 
@@ -62,21 +72,43 @@ module macarray (
     assign M = [7:4]    NMT;
     assign T = [3:0]    MNT;
 
-
+/*
     always @(posedge CLK or negedge RSTN) begin
         if(START == 1) begin
             if (readcount_I_flag < T) begin
                 readcount_I_flag <= readcount_I_flag + 1;
             end
+            // if (readcount_W_flag < M) begin
+            //     readcount_W_flag <= readcount_I_flag + 1;
+            // end
+        end
+    end
+*/
+//    assign ADDR_I = readcount_I_flag;
+
+
+
+/*
+    always @(posedge CLK or negedge RSTN) begin
+        if(START == 1) begin
             if (readcount_W_flag < M) begin
-                readcount_W_flag <= readcount_I_flag + 1;
+                if(weight_delay == 0) begin
+                    readcount_W_flag <= readcount_W_flag + 1;
+                end else begin
+                end
             end
         end
     end
 
-    assign ADDR_I = readcount_I_flag;
-    assign ADDR_W = readcount_W_flag;
+    assign EN_W     = (readcount_W_flag < M)    ?   ((weight_delay==0)   ?   1   :   0   ):   0;
+    assign ADDR_W   = readcount_W_flag;
+*/
 
+
+
+
+    // 생각해보니깐 W는 N에 다라서 변경할 필요가 없음
+    // 그리고 I는 N이 4미만일 때 변경할 필요가 없을 듯
     assign matrix_I =   (N == 0)    ?   {64'b0}                     :
                         (N == 1)    ?   {RDATA_I[63:56] , 56'b0}    :
                         (N == 2)    ?   {RDATA_I[63:48] , 48'b0}    :
@@ -98,26 +130,146 @@ module macarray (
                         (N == 8)    ?   {RDATA_W[63:0]}             :   {64'b0};
 
 
-    
     always @(posedge CLK or negedge RSTN) begin
         if(START == 1) begin
 
-            
+            if(N < 5) begin
+
+                if(T < 5) begin
+                    case (readcount_I_flag)
+                        0: begin
+                            {a11, a12, a13, a14} <= matrix_I[63:32];
+                        end
+                        1: begin
+                            {a21, a22, a23, a24} <= matrix_I[63:32];
+                        end
+                        2: begin
+                            {a31, a32, a33, a34} <= matrix_I[63:32];
+                        end
+                        3: begin
+                            {a41, a42, a43, a44} <= matrix_I[63:32];
+                        end
+                        default: begin
+                        // 필요한 경우 추가 작업
+                        end
+                    endcase
+
+                end else if (T >= 5) begin
+                    if(M >= 5) begin
+                        case (readcount_I_flag)
+                            0 , 4: begin
+                                {a11, a12, a13, a14} <= matrix_I[63:32];
+                            end
+                            1 , 5: begin
+                                {a21, a22, a23, a24} <= matrix_I[63:32];
+                            end
+                            2 , 6: begin
+                                {a31, a32, a33, a34} <= matrix_I[63:32];
+                            end
+                            3 , 7: begin
+                                {a41, a42, a43, a44} <= matrix_I[63:32];
+                            end
+                            default: begin
+                            // 필요한 경우 추가 작업
+                            end
+                        endcase 
+                    end else begin
+
+                    end
+
+                end
+
+
+
+            end else if (N >= 5) begin
+                if(T < 5) begin
+
+
+                end else if (T >= 5) begin
+
+
+                end
+            end
         end
     end
 
 
-/*    //START 신호에 따른 시작값
+/*
+                    case (readcount_I_flag)
+                        0: begin
+                            {a11, a12, a13, a14} <= matrix_I[63:32];
+                            {b11, b12, b13, b14} <= matrix_I[31:0];
+                        end
+                        1: begin
+                            {a21, a22, a23, a24} <= matrix_I[63:32];
+                            {b21, b22, b23, b24} <= matrix_I[31:0];
+                        end
+                        2: begin
+                            {a31, a32, a33, a34} <= matrix_I[63:32];
+                            {b31, b32, b33, b34} <= matrix_I[31:0];
+                        end
+                        3: begin
+                            {a41, a42, a43, a44} <= matrix_I[63:32];
+                            {b41, b42, b43, b44} <= matrix_I[31:0];
+                        end
+                        default: begin
+                        // 필요한 경우 추가 작업
+                        end
+                    endcase  
+*/
+/*  
     always @(posedge CLK or negedge RSTN) begin
         if(START == 1) begin
-            //MNT값을 통해 데이터 불러와서 레지스터에 저장하기
-            for (integer i = 0; i < T; i = i + 1) begin
-                matrix_I[i] <= [8*i+7:8*i] RDATA_I;
+            if(readcount_I_flag == 0) begin
+                a11 <= matrix_I[63:56];
+                a12 <= matrix_I[55:48];
+                a13 <= matrix_I[47:40];
+                a14 <= matrix_I[39:32];
+                b11 <= matrix_I[31:24];
+                b12 <= matrix_I[23:16];
+                b13 <= matrix_I[15:8];
+                b14 <= matrix_I[7:0];
+            end else if (readcount_I_flag == 1) begin
+                a21 <= matrix_I[63:56];
+                a22 <= matrix_I[55:48];
+                a23 <= matrix_I[47:40];
+                a24 <= matrix_I[39:32];
+                b21 <= matrix_I[31:24];
+                b22 <= matrix_I[23:16];
+                b23 <= matrix_I[15:8];
+                b24 <= matrix_I[7:0];
+            end else if (readcount_I_flag == 2) begin
+                a31 <= matrix_I[63:56];
+                a32 <= matrix_I[55:48];
+                a33 <= matrix_I[47:40];
+                a34 <= matrix_I[39:32];
+                b31 <= matrix_I[31:24];
+                b32 <= matrix_I[23:16];
+                b33 <= matrix_I[15:8];
+                b34 <= matrix_I[7:0];
+            end else if (readcount_I_flag == 3) begin
+                a41 <= matrix_I[63:56];
+                a42 <= matrix_I[55:48];
+                a43 <= matrix_I[47:40];
+                a44 <= matrix_I[39:32];
+                b41 <= matrix_I[31:24];
+                b42 <= matrix_I[23:16];
+                b43 <= matrix_I[15:8];
+                b44 <= matrix_I[7:0];
+            end else if (readcount_I_flag == 4) begin
+
+            end else if (readcount_I_flag == 5) begin
+
+            end else if (readcount_I_flag == 6) begin
+
+            end else if (readcount_I_flag == 7) begin
+
             end
-            
         end
     end
 */
+
+
 
 
 	
@@ -167,6 +319,9 @@ module macarray (
             a44 <= 0;
 
             readcount_I_flag <= 0;
+
+            weight_delay <= 0;
+            readcount_W_flag <= 0;
 
             cal_flag <=0;
         end
